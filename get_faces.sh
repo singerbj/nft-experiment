5f0d41e8d8a9fadf46953e81c1cc061e02e46fff
Benjamin Singer <benjmainjsinger@gmail.com>
2022-01-26 20:37:57 -0600

    init commit

---
+++ get_faces.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -rf ./images/faces/*
+
+for i in {1..20}
+do
+    wget https://www.thispersondoesnotexist.com/image --output-document "./images/faces/$i.png"
+    sleep 1
+done
+ 
